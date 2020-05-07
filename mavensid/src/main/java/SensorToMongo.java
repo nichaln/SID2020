9f1ea0a85224d2e0adae28921bb38e6249fc3a75
jmjmf-iscteiul <jmjmf@iscte-iul.pt>
2020-05-07 13:13:40 +0100

    Sensores

--- mavensid/src/main/java/SensorToMongo.java
+++ mavensid/src/main/java/SensorToMongo.java
@@ -14,18 +14,32 @@ public class SensorToMongo {
 
 	public void receberMensagem(String mensagem) {
 		String[] vetor = mensagem.split(",");
-		String[] aux;
-		String timestamp;
+		String[] parsed = new String[vetor.length];
+		for (int i = 0; i != vetor.length; i++) {
+			parsed[i] = vetor[i].split(":")[1].replaceAll("\"", "");
+		}
+		String timestamp = parsed[3]; // TODO valor correspondente do timestamp
 		Medicao m = new Medicao();
 		double leitura;
 		int i = 0;
-		for (String s : vetor) {
-			aux = s.split(":");
-			aux[1].replaceAll("\"", "");
+		for (String s : parsed) {
 			if (i == 0) {
-				verifyTmp(aux[1], 't');
-//				m.setMedicaoTemperatura(aux[1]);
+				if(verify(s, 't', timestamp))
+					m.setMedicaoTemperatura(Double.parseDouble(s));
 			}
+			if (i == 1) {
+				if(verify(s, 'h', timestamp))
+					m.setMedicaoTemperatura(Double.parseDouble(s));
+			}
+			// TODO Parse data
+			if (i == 3) {
+				if(verify(s, 'c', timestamp))
+					m.setMedicaoTemperatura(Double.parseDouble(s));
+			}
+			/*if (i == 3) {
+				if(verify(s, 't', timestamp))
+					m.setMedicaoTemperatura(Double.parseDouble(s));
+			}*/
 
 			i++;
 
@@ -33,27 +47,55 @@ public class SensorToMongo {
 
 	}
 
-	private void verifyTmp(String aux, char key,) {
+	private boolean verify(String aux, char key, String timestamp) {
 		switch (key) {
 		case 't':
+			double leitura;
 			try {
-				double leitura = Double.parseDouble(aux);
-			} catch (NumberFormatException e) {//escreve nos erros
-				LinkedList<MedicaoErroValores> historico = new LinkedList<MedicaoErroValores>();
-				for(Medicao m: medicoesAnteriores) {
-					historico.add(new MedicaoErroValores(m.getMedicaoTemperatura(),m.getDate()));
-				}
-				MedicaoErro me = new MedicaoErro(aux,historico);
-				
-				return;
+				leitura = Double.parseDouble(aux);
+			} catch (NumberFormatException e) {// escreve nos erros
+				MedicaoErro me = new MedicaoErro(aux, timestamp, history());
+
+				/*
+				 * TODO mandar esta me para o spot de guardar
+				 */
+
+				return false;
 			}
-			if() {
+			if (checkValues(leitura)) {// espetáculo regista aí
+				return true;
+			} else {// fora da graça de deus
+				MedicaoErro me = new MedicaoErro(aux, timestamp, history());
 				
+				/*
+				 * TODO mandar esta me para o spot de guardar
+				 */
+				return false;
 			}
 //				
 		case 'h':
 		case 'c':
+		default:
+			return false;
+			
 		}
+	}
+
+	private LinkedList<MedicaoErroValores> history() {
+		LinkedList<MedicaoErroValores> historico = new LinkedList<MedicaoErroValores>();
+		for (Medicao m : medicoesAnteriores) {
+			historico.add(new MedicaoErroValores(m.getMedicaoTemperatura(), m.getDate()));
+		}
+		return historico;
+	}
+
+	private boolean checkValues(Double leitura) {
+		if (leitura > 20564) { // Comparação do demónio
+			return true;
+		} else {
+			return false;
+		}
+
 	}
 
 	public static void main(String[] args) {
